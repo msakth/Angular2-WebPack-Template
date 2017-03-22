475ab3abe626806269fb65018be63477bba1937d
msakth <rushtomuthu@hotmail.com>
2017-03-22 23:08:17 +0000

    Committing the core angular2 template

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import '../assets/css/styles.css';
+import {} from '@types/node';
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
